6b7e30eec087a4b1a72fb07879c1c7397e83b376
Sabrina <sabrinaferreira123@hotmail.com>
2024-03-18 14:14:59 -0300

    corrigindo main

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from utils import (
     running_all_instance_with_chosen_capacity,
 )
 from context import ProjectContext
-from zero_formulation import build_model as classical_formulation_build_model
-from first_formulation import build_model as first_formulation_build_model
-from second_formulation import build_model as second_formulation_build_model
-from third_formulation import build_model as third_formulation_build_model
-from fourth_formulation import build_model as fourth_formulation_build_model
+# from zero_formulation import build_model as classical_formulation_build_model
+# from first_formulation import build_model as first_formulation_build_model
+# from second_formulation import build_model as second_formulation_build_model
+# from third_formulation import build_model as third_formulation_build_model
+# from fourth_formulation import build_model as fourth_formulation_build_model
 from fifth_formulation import build_model as fifth_formulation_build_model
 
 if __name__ == "__main__":
